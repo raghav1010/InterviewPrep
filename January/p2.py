2eb26250558780d541f948af089104d8a2c79d5e
raghabendra <raghavkol1997@gmail.com>
2022-01-02 18:28:20 +0530

    day2

--- January/p2.py
+++ January/p2.py
@@ -1,1 +1,9 @@
-#100
+#1010 Pairs of Songs With Total Durations Divisible by 60
+class Solution:
+    def numPairsDivisibleBy60(self, time) -> int:
+        mapp = [0] * 60
+        result = 0
+        for x in time:
+            result += mapp[-x % 60]
+            mapp[x % 60] += 1
+        return result
