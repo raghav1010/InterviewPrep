e57aca9ccb0a5243e56fad4f5795ab3596ba05e1
raghabendra <raghavkol1997@gmail.com>
2022-03-01 23:06:26 +0530

    March day 1

---
+++ March/1.py
@@ -0,0 +1,17 @@
+#338. Counting Bits
+class Solution:
+    def countBits(self, n: int) -> list[int]:
+        ans = [0] * (n + 1)
+        x = 0
+        b = 1
+        
+        # [0, b) is calculated
+        while b <= n:
+            # generate [b, 2b) or [b, n) from [0, b)
+            while x < b and x + b <= n:
+                ans[x + b] = ans[x] + 1
+                x += 1
+            x = 0 # reset x
+            b <<= 1 # b = 2b
+            
+        return ans
